6c65ce0446f61527ac4cc483970b47c430fe5f2b
migue <mrivasperez@icloud.com>
2023-11-10 12:18:54 -0800

    chore: Code Hygiene

--- src/features/LetterDetail/LetterDetail.tsx
+++ src/features/LetterDetail/LetterDetail.tsx
@@ -2,7 +2,6 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {alphabet_es} from '../../alphabet/es';
 import {RootStackParamList} from '../../App';
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RouteProp, useRoute} from '@react-navigation/native';
 
 const styles = StyleSheet.create({});
@@ -13,6 +12,7 @@ const LetterDetail = () => {
   const letterData = alphabet_es[route.params.letterIndex];
 
   console.log(letterData);
+
   return (
     <View>
       <Text>LetterDetail</Text>
