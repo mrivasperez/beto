6bb1a2883f7adcac3dd244f0af366c0640c6d72c
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-22 13:39:49 -0800

    refactor: export alphabet grid to alphabet-grid component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { alphabet } from './alphabet';
-import { OnboardingComponent } from "./onboarding/onboarding.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, OnboardingComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'beto';
-  alphabet = alphabet;
-  selectedLetter: string | null = null;
 
-  onLetterClick(letter: string) {
-    this.selectedLetter = letter;
-    let textToSpeak = letter;
-    if (letter === 'y') {
-      textToSpeak = "i griega"
-    }
-    const utterance = new SpeechSynthesisUtterance(textToSpeak);
-    utterance.lang = 'es-US'
-    window.speechSynthesis.speak(utterance);
-  }
 }
