f88cc65237767194b78228090bf9965a4f7504ca
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-23 11:16:43 -0800

    task: scaffold new WordList component

---
+++ src/app/words-list/words-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-words-list',
+  imports: [],
+  templateUrl: './words-list.component.html',
+  styleUrl: './words-list.component.css'
+})
+export class WordsListComponent {
+
+}
