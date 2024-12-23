699e8a73e097841d34c26739d7cf476bd7984bc2
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-22 16:42:33 -0800

    task: scaffold word component

---
+++ src/app/shared/word/word.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-word',
+  imports: [],
+  templateUrl: './word.component.html',
+  styleUrl: './word.component.css'
+})
+export class WordComponent {
+
+}
