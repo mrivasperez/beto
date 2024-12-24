94c6c9636120f0a00e46db35f7c860f7989491a8
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-24 09:07:22 -0800

    task: create getWordsForLetter in WordsService

--- src/app/words-list/words.service.ts
+++ src/app/words-list/words.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WordsService {
-
-  constructor() { }
+  async getWordsForLetter(
+    letter: string
+  ): Promise<{ word: string; emoji: string }[]> {
+    try {
+      const module = await import(`../data/spanish/words/${letter}.ts`);
+      return module.words;
+    } catch (error) {
+      console.error(`Error loading words for ${letter}:`, error);
+      return [];
+    }
+  }
 }
