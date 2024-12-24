f0707c13c155b0fe0d78fc264aaf2ecce23ad31f
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-24 10:07:40 -0800

    task: load word list in word-list component

--- src/app/words-list/words-list.component.ts
+++ src/app/words-list/words-list.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { WordsService } from './words.service';
 
 @Component({
   selector: 'app-words-list',
   imports: [],
   templateUrl: './words-list.component.html',
-  styleUrl: './words-list.component.css'
+  styleUrl: './words-list.component.css',
 })
-export class WordsListComponent {
+export class WordsListComponent implements OnInit {
+  letter: string | null = null;
+  words: { word: string; emoji: string }[] = [];
 
+  constructor(
+    private route: ActivatedRoute,
+    private wordsService: WordsService
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    this.route.params.subscribe(async (params) => {
+      this.letter = params['letter'];
+      if (this.letter) {
+        this.words = await this.wordsService.getWordsForLetter(this.letter);
+      }
+    });
+  }
 }
