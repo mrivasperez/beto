f93013f2e0c98ea4f8064eeb6b66884aa98d85a7
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-27 11:35:31 -0800

    task: scaffold voice component

---
+++ src/app/voice/voice.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voice',
+  imports: [],
+  templateUrl: './voice.component.html',
+  styleUrl: './voice.component.css'
+})
+export class VoiceComponent {
+
+}
